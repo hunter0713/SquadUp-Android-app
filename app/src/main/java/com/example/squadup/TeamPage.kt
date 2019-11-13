990a36d50ee8da706097b24815cdcb101f092d86
Zack Khazraeinazmpour <zack_khaz@ku.edu>
2019-11-13 14:16:55 -0600

    Adding gradle scripts

--- app/src/main/java/com/example/squadup/TeamPage.kt
+++ app/src/main/java/com/example/squadup/TeamPage.kt
@@ -8,5 +8,6 @@ class TeamPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_team_page)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
     }
 }
