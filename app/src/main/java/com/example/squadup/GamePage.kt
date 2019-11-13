8dc057db99219de8f4dd0cb39797fd71c4253498
Zack Khazraeinazmpour <zack_khaz@ku.edu>
2019-11-13 14:34:30 -0600

    Added another activity page that the actual game will take place on

---
+++ app/src/main/java/com/example/squadup/GamePage.kt
@@ -0,0 +1,12 @@
+package com.example.squadup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GamePage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_page)
+    }
+}
