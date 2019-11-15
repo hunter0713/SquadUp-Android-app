4e5608ed41b98266b658d8fb822991402419e24a
zackkhaz1 <zack_khaz@ku.edu>
2019-11-15 14:31:01 -0600

    Added code to store entered team Members, fixed many "use string resource" warnings in XML file. Working on getting radio button and team data to next Activity

--- app/src/main/java/com/example/squadup/TeamPage.kt
+++ app/src/main/java/com/example/squadup/TeamPage.kt
@@ -5,14 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_team_page.*
 import kotlinx.android.synthetic.main.content_main.*
 
 class TeamPage : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_team_page)
-
-        }
+        val teamIntent = Intent(this@TeamPage, GamePage::class.java)
+        val member1 = editText.getText().toString()
+        val member2 = editText5.getText().toString()
+        val member3 = editText5.getText().toString()
+        val member4 = editText7.getText().toString()
+        val member5 = editText8.getText().toString()
+        teamIntent.putExtra("Member1", member1)
+        teamIntent.putExtra("Member2", member2)
+        teamIntent.putExtra("Member3", member3)
+        teamIntent.putExtra("Member4", member4)
+        teamIntent.putExtra("Member5", member5)
+    }
     }
 
